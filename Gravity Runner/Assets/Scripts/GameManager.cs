e91ac685432f4c4612087da8236d991a3da48395
Bernard <berdzhart@gmail.com>
2024-11-17 21:08:16 +0700

    Revert "Version That Doesnt Work"
    This reverts commit 7938d2d92d6e7bf190bf4158cda9d9e66546bbc6.

---
+++ Gravity Runner/Assets/Scripts/GameManager.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    Canvas mainUI;
+    Canvas endScreen;
+    public PlayerDeath playerDeath;
+
+    Canvas pauseScreen;
+
+    public bool playerHasDied;
+
+    public Button jumpButton;
+
+    void Awake()
+    {
+        mainUI = GameObject.Find("Main Canvas").GetComponent<Canvas>();
+        endScreen = GameObject.Find("GameOver").GetComponent<Canvas>();
+        pauseScreen = GameObject.Find("PauseScreen").GetComponent<Canvas>();
+        playerDeath = GameObject.Find("Player").GetComponent<PlayerDeath>();
+        mainUI.gameObject.SetActive(true);
+        endScreen.gameObject.SetActive(false);
+        pauseScreen.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {       
+        if (playerDeath.playerHasDied == true)
+        {
+            mainUI.gameObject.SetActive(false);
+            endScreen.gameObject.SetActive(true);
+        }
+
+        else if (playerDeath.playerHasDied == false)
+        {
+            if(Input.GetKeyDown (KeyCode.Escape)) 
+            {
+                if (!pauseScreen.gameObject.activeInHierarchy) 
+                {
+                    PauseGame();
+                }
+                else if (pauseScreen.gameObject.activeInHierarchy) 
+                {
+                    ContinueGame();
+                }
+            } 
+        }
+
+        if(!pauseScreen.gameObject.activeInHierarchy)
+        {
+            jumpButton.gameObject.SetActive(true);
+        }
+        else if (pauseScreen.gameObject.activeInHierarchy)
+        {
+            jumpButton.gameObject.SetActive(false);
+        }
+        
+    }
+
+    public void OnReplayLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void OnResumeLevel()
+    {
+        Time.timeScale = 1;
+        pauseScreen.gameObject.SetActive(false);
+        jumpButton.gameObject.SetActive(true);
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        pauseScreen.gameObject.SetActive(true);
+        
+    } 
+    public void ContinueGame()
+    {
+        Time.timeScale = 1;
+        pauseScreen.gameObject.SetActive(false);
+    }
+}
